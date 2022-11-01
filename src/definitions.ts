d1b037c55c1ad066059cdec64e9a81cdf96cd2bf
marin <marin.glavas@outlook.com>
2022-11-01 20:32:59 +0100

    add plugin definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,17 @@
 export interface BluetoothSerialPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  connect(options: { address: string }): Promise<void>;
+  disconnect(): Promise<void>;
+  read(): Promise<{ data: string }>;
+  write(options: { data: string }): Promise<void>;
+  available(): Promise<boolean>;
+  isEnabled(): Promise<boolean>;
+  isConnected(): Promise<boolean>;
+  clear(): Promise<void>;
+  enable(): Promise<{ isEnabled: boolean }>;
+  settings(): Promise<void>;
+  list(): Promise<any[]>;
+  discover(): Promise<any[]>;
+  checkPermissions(options: { permissions: string[] }): Promise<any[]>;
+  requestPermissions(options: { permissions: string[] }): Promise<any[]>;
 }
