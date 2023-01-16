e162113d558c5f4e5cd61d6128e59ecb9fb0dfca
Glavotaner <60708947+Glavotaner@users.noreply.github.com>
2023-01-16 09:29:22 +0000

    fix discovery permissions

--- android/src/main/java/com/glavotaner/bluetoothserial/BluetoothSerialPlugin.java
+++ android/src/main/java/com/glavotaner/bluetoothserial/BluetoothSerialPlugin.java
@@ -64,8 +64,8 @@ public class BluetoothSerialPlugin extends Plugin {
     private PluginCall writeCall;
     private PluginCall discoveryCall;
     private BroadcastReceiver discoveryReceiver;
+    private List<String> discoveryPermissions;
     private boolean requiresLocationForDiscovery = true;
-    private final List<String> discoveryPermissions = new ArrayList<>();
 
     StringBuffer buffer = new StringBuffer();
 
@@ -113,14 +113,16 @@ public class BluetoothSerialPlugin extends Plugin {
 
     private void setDiscoveryPermissions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            discoveryPermissions.add(SCAN);
+            // SCAN - required for just invoking discovery methods
+            // CONNECT - required for getting data about a discovered device
+            discoveryPermissions = List.of(SCAN, CONNECT);
             if (getConfig().getBoolean("neverScanForLocation", false)) {
                 requiresLocationForDiscovery = false;
             } else {
                 discoveryPermissions.add(FINE_LOCATION);
             }
         } else {
-            discoveryPermissions.add(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q ? FINE_LOCATION : COARSE_LOCATION);
+            discoveryPermissions = List.of(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q ? FINE_LOCATION : COARSE_LOCATION);
         }
     }
 
@@ -332,8 +334,7 @@ public class BluetoothSerialPlugin extends Plugin {
             discoveryReceiver = null;
         }
     }
-
-    @SuppressLint("MissingPermission")
+    
     private void startDiscovery(PluginCall call) {
         cancelDiscovery();
         discoveryCall = call;
@@ -455,11 +456,7 @@ public class BluetoothSerialPlugin extends Plugin {
             List<String> requestedPermissions = call.getArray("permissions").toList();
             JSObject permissions = new JSObject();
             for (String alias: requestedPermissions) {
-                if (alias.contains("Location")) {
-                    permissions.put(alias, getPermissionState(alias));
-                } else {
-                    permissions.put(alias, PermissionState.GRANTED);
-                }
+                permissions.put(alias, alias.contains("Location") ? getPermissionState(alias) : PermissionState.GRANTED);
             }
             call.resolve(permissions);
         } catch(JSONException exception) {
